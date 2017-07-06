31ceb14cbf51ba41e8c4940bf2d4b9580e5cc9c3
sanlouise <sandra.adamshallie@gmail.com>
2017-07-06 16:17:01 -0400

    Add basic JS logic for setup of the game

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
-function start() {
+let start = () => {
   document.activePlayer = "X";
+  setText(document.activePlayer + " can start!")
 }
 
-function playerChoice(square) {
-  square.innerText = document.activePlayer;
-  changeTurn();
+let setText = (message) => {
+  document.getElementById("user-message").innerText = message;
 }
 
-function changeTurn() {
-  return document.activePlayer === "X" ? document.activePlayer = "O" : document.activePlayer = "X";
+let playerChoice = (square) => {
+  if (square.innerText=="") {
+    square.innerText = document.activePlayer;
+    changeTurn();
+  } else {
+    setText("Choose another move...");
+  }
 }
+
+let changeTurn = () => {
+  if (document.activePlayer === "X") {
+    document.activePlayer = "O"
+  }
+  else {
+    document.activePlayer = "X"
+  }
+  setText("It is now " + document.activePlayer + " time!");
+  return;
+}
+
+
+let getBox = (number) => {
+  return document.getElementById("square" + number).innerText;
+} 
